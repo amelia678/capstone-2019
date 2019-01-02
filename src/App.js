b17bacabfd71de609ab8f6a4681bfd121ca6d8a7
Amelia Schulz <amelia.schulz@gmail.com>
2019-01-02 11:20:08 -0500

    adds Search component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+import Search from './Search';
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>MusicPal</h1>
-
+        <Search />
       </div>
     );
   }
---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Search = (props) => {
+    return (
+        <div className="searchBar">
+            <h2>Search events by artist, genre, or city:</h2>
+            <input></input>
+            <button>Search</button>
+        </div>
+
+    )
+}
+
+export default Search;
