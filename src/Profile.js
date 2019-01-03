d26ad70e0ad4955001bb3a973d68dddb0d9169b4
Clare Barton <grimalkinclare@gmail.com>
2019-01-03 15:54:03 -0500

    makes them talk to each other YAAASSSS

--- src/Profile.js
+++ src/Profile.js
@@ -1,37 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import UpcomingShows from './UpcomingShows';
 import FavoriteArtists from './FavoriteArtists';
 
 class Profile extends Component {
     constructor(props) {
-        super(props); 
+        super(props);
         this.state = {
-            myArtists : [
-              'Drake',
-              'Grimes',
-              'The strokes',
-              'Sza'
+            myArtists: [
+                'Drake',
+                'Grimes',
+                'The strokes',
+                'Sza'
             ],
-            myShows : [
-              'Drake: StateFarm Arena',
-              'Noname: Masquerade',
-              'BloodOrange: Variety Playhouse'
-              
+            myShows: [
+                'Drake: StateFarm Arena',
+                'Noname: Masquerade',
+                'BloodOrange: Variety Playhouse'
+
             ]
-          }
+        }
     }
     render() {
         return (
-        <div className="userProfile">
-            <h1>My  Profile</h1>
-            <UpcomingShows
-            myShows={this.state.myShows} />
-            <FavoriteArtists
-            myArtists= {this.state.myArtists} />
-        </div>
+            <div className="userProfile">
+                <h1>My Profile</h1>
+                <UpcomingShows
+                    myShows={this.state.myShows} />
+                <FavoriteArtists
+                    myArtists={this.state.myArtists} />
+            </div>
         )
     }
-    
+
 }
 
 
--- src/UpcomingShows.js
+++ src/UpcomingShows.js
@@ -1,20 +1,42 @@
 import React from 'react';
 
-const UpcomingShows = (props) => {
-    const showList = props.myShows.map(show => {
+class UpcomingShows extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            myShows: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('/upcomingShows')
+            .then(r => r.json())
+            .then(showArray => {
+                // console.log(showArray);
+                this.setState({
+                    myShows: showArray
+                })
+            })
+    };
+
+    render() {
+
+        const showList = this.state.myShows.map(show => {
+            console.log(show);
+            return (
+                <li key={show.id}>{show.artist}</li>
+            )
+        })
+        // console.log(this.state.myShows);
         return (
-            <li>{show}</li>
+            <div>
+                <p>My Shows:</p>
+                <ul>
+                    {showList}
+                </ul>
+            </div>
         )
-    })
-    return (
-        <div>
-            <p>My Shows:</p>
-            <ul>
-              
-                {showList}
-            </ul>
-        </div>
-    )
+    }
 }
 
 export default UpcomingShows;
