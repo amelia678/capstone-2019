a4208ac51c0f1b745d8c51d7750dfae14052c121
Clare Barton <grimalkinclare@gmail.com>
2019-01-04 16:04:05 -0500

    makes api call event info into one object which we're going to move to the backend anyway

--- src/EventfulCall.js
+++ src/EventfulCall.js
@@ -6,7 +6,11 @@ class EventfulCall extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            eventArray: [],
+            // artistName: '',
+            // venue: '',
+            // location: '',
+            // date: ''
         }
     }
 
@@ -15,13 +19,39 @@ class EventfulCall extends React.Component {
         fetch(`http://my-little-cors-proxy.herokuapp.com/http://api.eventful.com/json/events/search?app_key=${API_KEY}&keywords=concert+music&location=Atlanta+GA&date=This+Weekend`)
             .then(r => r.json())
             .then(data => {
-                console.log(data.events.event[0].title);
+                // this.setState({
+                const apiList = [
+                    {
+                        artist: data.events.event[0].title,
+                        venue: data.events.event[0].venue_name,
+                        city: data.events.event[0].city_name,
+                        state: data.events.event[0].region_abbr,
+                        date: data.events.event[0].start_time
+                    }
+                ];
+                return apiList
+                // })
+            })
+            .then(apiList => {
+                this.setState({
+                    eventArray: apiList
+                })
             })
     }
 
     render() {
+
+        const eventList = this.state.eventArray.map(event => {
+            console.log(event);
+            return (
+                <li>{event.artist} {event.venue} {event.city} {event.state} {event.date}</li>
+            )
+        })
+
         return (
-            <p>more events</p>
+            <ul>
+                {eventList}
+            </ul>
         )
     }
 
