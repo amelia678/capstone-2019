cbaf4e07b8af92cb6eff5c78f6d0b69a5eff118b
Amelia Schulz <amelia.schulz@gmail.com>
2019-01-07 09:42:28 -0500

    adds registration form to Login component

--- src/Login.js
+++ src/Login.js
@@ -3,20 +3,46 @@ import React from 'react';
 const Login = (props) => {
     return (
         <div>
-            {/* <h2>this is the login</h2> */}
-        <form>
+        <form action="" method="POST">
             <label>
         <span >Username:</span>
             <input type="text" name="username"></input>
         </label>
-        {/* <br> </br> */}
+        
         <label>
         <span>Password:</span>
             <input type="password" name="password"></input>
         </label>
-        {/* <br> </br> */}
+        
         <input class="aqua" type="submit" value="Login"></input>
-        </form>    
+        </form> 
+        <h3>Not a member yet? Register an account here:</h3>   
+            <form action="" method="POST">
+                <label><span class="shadow"> Your name:</span>
+                    <input type="text" name="name"></input>
+                </label>
+                <label> <span class="shadow">Username:</span>
+                <input type="text" name="username"></input>
+                </label>
+                <label><span class="shadow"> Password:</span>
+                    <input type="password" name="password"></input>
+                </label>
+                <label> <span class="shadow">Email:</span>
+                    <input type="email" name="email"></input>
+                </label>
+                
+                <label> <span class="shadow">City:</span>
+                    <input type="text" name="city"></input>
+                </label>
+                 
+                <label> <span class="shadow">State:</span>
+                    <input type="text" name="state"></input>
+                </label>	
+                		
+                <label>
+                <input type="submit" value="Sign Up!"></input>
+                </label>
+            </form>
         </div>
     )
 }
