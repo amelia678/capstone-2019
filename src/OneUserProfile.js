00ed24c155523554acb6771aa1cbcef1a44db695
Clare Barton <grimalkinclare@gmail.com>
2019-01-12 10:07:46 -0500

    makes oneuserprofile component

---
+++ src/OneUserProfile.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class OneUserProfile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    ComponentDidMount() {
+        fetch('/friendProfile')
+
+            .then(r => r.json())
+            .then(user => {
+
+                this.setState({
+                    username: user.username,
+                    home: user.userHome
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.username}'s Profile</h1>
+                <p>About Me:</p>
+                <ul >
+                    <li>{this.state.username}</li>
+                    <li>{this.state.home}</li>
+                </ul>
+                <UpcomingShows
+                    myShows={this.state.myShows} />
+                <Link to="/home">Search More Shows</Link>
+                <FavoriteArtists
+                    myArtists={this.state.myArtists} />
+                <Link to="/search-artists">Search More Artists</Link>
+                <MyFriends
+                    myFriends={this.state.myFriends} />
+                <Link className="logout" to="/logout">Logout</Link>
+            </div>
+        )
+    }
+}
+
+export default OneUserProfile;
